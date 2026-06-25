eb22a3bf26d2aef09365bd242f8f6654936d2ccb
StewLemin <mateidima@gmail.com>
2026-06-25 19:46:19 +0200

    Symbol Table is mostly done

--- src/main/java/ut/pp/compiler/checker/Symbol.java
+++ src/main/java/ut/pp/compiler/checker/Symbol.java
@@ -1,4 +1,24 @@
 package ut.pp.compiler.checker;
 
+import ut.pp.ast.type.TypeNode;
+
 public class Symbol {
+    private final String name;
+    private final TypeNode type;
+    private boolean initialized = false;
+
+    public Symbol(String name, TypeNode type, boolean initialized){
+        this.name = name;
+        this.type = type;
+        this.initialized = initialized;
+    }
+
+    public void initialize(){
+        initialized = true;
+    }
+
+    public boolean isInitialized(){
+        return initialized;
+    }
+
 }
