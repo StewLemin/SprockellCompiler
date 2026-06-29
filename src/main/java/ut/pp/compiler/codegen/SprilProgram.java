d3f81bfe1953ad1b3e80714257daa746cadb1b25
StewLemin <mateidima@gmail.com>
2026-06-30 00:35:52 +0200

    Spril and SprilProgram

--- src/main/java/ut/pp/compiler/codegen/SprilProgram.java
+++ src/main/java/ut/pp/compiler/codegen/SprilProgram.java
@@ -1,4 +1,31 @@
 package ut.pp.compiler.codegen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SprilProgram {
+    public List<String> instructions = new ArrayList<>();
+
+    public SprilProgram(){
+        this.instructions = new ArrayList<>();
+    }
+
+    public int emit(String instruction){
+        instructions.add(instruction);
+        return instructions.size() - 1;
+    }
+
+    public int size(){
+        return instructions.size();
+    }
+
+    public void patch(String instruction, int index){
+        instructions.set(index,instruction);
+    }
+
+    public List<String> getInstruction(){
+        return instructions;
+    }
+
+
 }
